c0f9b9257ac0e1e3377b18530d937496edee630e
albunus <albunus.nyalita@student.moringaschool.com>
2021-11-06 12:46:05 +0300

    initializing my app

--- virtual/bin/activate.fish
+++ virtual/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/access/Desktop/python/pith_app/virtual"
+set -gx VIRTUAL_ENV "/home/access/Desktop/python/pitch_app/virtual"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
