216dbbe5c9bd1d6957f8af41c4c82389ebb634f1
albunus <albunus.nyalita@student.moringaschool.com>
2021-11-06 21:52:15 +0300

    adding method to compare user passwords

--- app/models.py
+++ app/models.py
@@ -68,5 +68,13 @@ class User(UserMixin,db.Model):
 
 
 
+@property
+def password(self):
+    raise AttributeError("You cannot read the password attribute")
+@password.setter
+def password(self,password):
+    self.password_hash = generate_password_hash(password)
 
+def verify_password(self,password):
+    return check_password_hash(self.password_hash,password)
 
