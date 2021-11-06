529f026b72af1bccc9ffc8a01419344802ce15ea
albunus <albunus.nyalita@student.moringaschool.com>
2021-11-06 21:50:23 +0300

    creating user class for relationships

--- app/models.py
+++ app/models.py
@@ -40,3 +40,33 @@ class Category(db.Model):
     categories = Category.query.all()
     return categories
 
+class Comments(db.Model):
+  __tablename__ = "comments"
+  id = db.Column(db.Integer,primary_key = True)
+  body = db.Column(db.String(255))
+  pitch_id = db.Column(db.Integer,db.ForeignKey('pitches.id'))  
+  user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+
+  def save_comment(self):
+    db.session.add(self)
+    db.session.commit()
+
+  @classmethod
+  def get_comment(self,id):
+    comments = Comments.query.filter_by(pitch_id =id).all()
+    return comments
+
+class User(UserMixin,db.Model):
+  __tablename__ = 'users'
+
+  id = db.Column(db.Integer,primary_key= True)
+  username = db.Column(db.String(255))
+  email = db.Column(db.String(255),unique = True, index = True)
+  password_hash = db.Column(db.String(255))
+  pitches = db.relationship("Pitch", backref = 'user', lazy = 'dynamic')
+  comments = db.relationship("Comments", backref = 'user', lazy = 'dynamic')
+
+
+
+
+
