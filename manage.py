34debf0bd34bbf7140c50185db838c9eef63b08d
albunus <albunus.nyalita@student.moringaschool.com>
2021-11-08 22:23:53 +0300

    update

--- manage.py
+++ manage.py
@@ -7,11 +7,13 @@ from flask_migrate import Migrate, MigrateCommand
 
 # Creating app instance
 app = create_app('development')
+app = create_app('test')
 
 manager = Manager(app)
-migrate = Migrate(app, db)
-
 manager.add_command('server', Server)
+
+
+migrate = Migrate(app, db)
 manager.add_command('db', MigrateCommand)
 
 
